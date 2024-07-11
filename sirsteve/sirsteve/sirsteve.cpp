693fde0c52c24ff9a1454df2d78a37ff05c68f9d
MLmihz <167431548+MLmihz@users.noreply.github.com>
2024-07-11 10:51:46 +0300

    Update sirsteve.cpp

--- sirsteve/sirsteve/sirsteve.cpp
+++ sirsteve/sirsteve/sirsteve.cpp
@@ -73,7 +73,7 @@ void addStudent() {
     cout << "Enter Gender (Male/Female): ";
     cin >> student.gender;
     if (student.gender !="male" && student.gender!= "female")
-     { cout << "Invalid gender. Please try again"<<endl;return;}
+     { cout << "Invalid gender. Please try again"<<endl;return addstudent();}
     cout << "Enter Age: ";
     cin >> student.age;
     cout << "Enter BBIT Group (1-3): ";
